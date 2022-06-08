1bb8729113e04e131bf727fc85d86ecb4df40cff
RaulPPealez <raul.perez@uam.es>
2022-06-08 17:55:38 +0200

    PSE: Add missing file

--- solvers/PSE/python_wrapper.cpp
+++ solvers/PSE/python_wrapper.cpp
@@ -5,9 +5,5 @@
 MOBILITY_PYTHONIFY_WITH_EXTRA_CODE(PSE,
 				   solver.def("setParametersPSE",
 					      &PSE::setParametersPSE,
-					      "psi"_a, "Splitting parameter", "Lx"_a, "Ly"_a, "Lz"_a);,
+					      "psi"_a, "Splitting parameter", "Lx"_a, "Ly"_a, "Lz"_a, "shearStrain"_a);,
   "This module computes the RPY mobility in triply periodic boundaries using Ewald splitting in the GPU.");
-
-
-
-
