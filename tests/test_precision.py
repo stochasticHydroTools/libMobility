aec74a3095f5f33e66ee0970e5aa2019cdb5cfef
Ryker Fish <rfish@mines.edu>
2024-04-29 19:08:52 -0600

    added tests to check functionality based on the compiled libmobility precision

---
+++ tests/test_precision.py
@@ -0,0 +1,79 @@
+import pytest
+import numpy as np
+
+from libMobility import *
+
+sane_parameters = {
+    "PSE": {"psi": 1.0, "Lx": 32, "Ly": 32, "Lz": 32, "shearStrain": 0.0},
+    "NBody": {"algorithm": "advise"},
+    "SelfMobility": {"parameter": 5.0},
+}
+
+
+@pytest.mark.parametrize(
+    ("Solver", "periodicity"),
+    [(SelfMobility, ("open", "open", "open"))],
+)
+def test_precision(Solver, periodicity):
+    # tests that Mdot gives a non-zero result when using the precision that libMobility was compiled in
+
+    hydrodynamicRadius = 1.0
+
+    solver = Solver(*periodicity)
+    solver.setParameters(**sane_parameters[Solver.__name__])
+    numberParticles = 1
+    solver.initialize(
+        temperature=1.0,
+        viscosity=1.0,
+        hydrodynamicRadius=hydrodynamicRadius,
+        numberParticles=numberParticles,
+    )
+
+    # Set precision to be the same as compiled precision
+    precision = np.float32 if Solver.precision == "float" else np.float64
+    positions = np.random.rand(numberParticles, 3).astype(precision)
+
+    size = 3 * numberParticles
+
+    solver.setPositions(positions)
+    forces = np.ones(size, dtype=precision)
+    mf = np.zeros(size, dtype=precision)
+    solver.Mdot(forces, mf)
+
+    zeros = np.zeros(size, dtype=precision)
+    assert (
+        mf != zeros
+    ).all(), "Mdot should not come out to be zero when using correct precision."
+
+
+@pytest.mark.parametrize(
+    ("Solver", "periodicity"),
+    [(SelfMobility, ("open", "open", "open"))],
+)
+def test_incorrect_precision(Solver, periodicity):
+    # tests that Mdot gives an error when inputs are in the wrong precision
+
+    hydrodynamicRadius = 1.0
+
+    solver = Solver(*periodicity)
+    solver.setParameters(**sane_parameters[Solver.__name__])
+    numberParticles = 1
+    solver.initialize(
+        temperature=1.0,
+        viscosity=1.0,
+        hydrodynamicRadius=hydrodynamicRadius,
+        numberParticles=numberParticles,
+    )
+
+    # Set precision to be opposite from compiled precision
+    precision = np.float32 if Solver.precision != "float" else np.float64
+    positions = np.random.rand(numberParticles, 3).astype(precision)
+
+    size = 3 * numberParticles
+
+    solver.setPositions(positions)
+    forces = np.ones(size, dtype=precision)
+    mf = np.zeros(size, dtype=precision)
+
+    with pytest.raises(RuntimeError):
+        solver.Mdot(forces, mf)
