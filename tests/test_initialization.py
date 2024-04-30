52d6cde40718bc882446ada940abdc42f11192f1
Ryker Fish <rfish@mines.edu>
2024-04-30 10:31:00 -0600

    added tests to check nbody batch parameters

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -25,3 +25,43 @@ def test_periodic_initialization(Solver, periodicity):
 def test_invalid_throws(Solver):
     with pytest.raises(RuntimeError):
         solver = Solver("periodicasdas", "periodic", "open")
+
+@pytest.mark.parametrize(("NBatch", "NperBatch"),
+                         [
+                            (1, 12),
+                            (12, 1),
+                         ])
+def test_nbody_good_batch_parameters(NBatch, NperBatch):
+    solver = NBody("open", "open", "open")
+    solver.setParameters("advise", NBatch, NperBatch)
+    numberParticles = NBatch*NperBatch
+    solver.initialize(
+        temperature=1.0,
+        viscosity=1.0,
+        hydrodynamicRadius=1.5,
+        numberParticles=numberParticles)
+    
+def test_nbody_default_parameters():
+    solver = NBody("open", "open", "open")
+    solver.setParameters("advise")
+    numberParticles = 10
+    solver.initialize(
+        temperature=1.0,
+        viscosity=1.0,
+        hydrodynamicRadius=1.5,
+        numberParticles=numberParticles)
+    
+@pytest.mark.parametrize(("NBatch", "NperBatch", "numberParticles"),
+                         [
+                            (5, 1, 10),
+                            (2, -1, 8),
+                         ])
+def test_nbody_bad_parameters(NBatch, NperBatch, numberParticles):
+    solver = NBody("open", "open", "open")
+    solver.setParameters("advise", NBatch, NperBatch)
+    with pytest.raises(RuntimeError):
+        solver.initialize(
+            temperature=1.0,
+            viscosity=1.0,
+            hydrodynamicRadius=1.5,
+            numberParticles=numberParticles)
