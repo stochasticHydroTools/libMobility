d3579724899d3a8d7a6f257d42042c73725d8438
Ryker Fish <rfish@mines.edu>
2024-10-10 15:53:32 -0600

    pulled out solver initialization into a function to reduce boilerplate and add test to check if positions have been set before Mdot to prevert soft failures or obscure cuda memory bug

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,7 +1,7 @@
 import pytest
 from libMobility import *
 import numpy as np
-from utils import sane_parameters
+from utils import sane_parameters, initialize_solver
 
 
 @pytest.mark.parametrize(
@@ -16,17 +16,9 @@ from utils import sane_parameters
     ],
 )
 def test_contiguous(Solver, periodicity):
-    hydrodynamicRadius = 1.0
-    solver = Solver(*periodicity)
-    parameters = sane_parameters[Solver.__name__]
-    solver.setParameters(**parameters)
+
     numberParticles = 1
-    solver.initialize(
-        temperature=1.0,
-        viscosity=1.0,
-        hydrodynamicRadius=hydrodynamicRadius,
-        numberParticles=numberParticles,
-    )
+    solver = initialize_solver(Solver, periodicity, numberParticles)
 
     # Set precision to be the same as compiled precision
     precision = np.float32 if Solver.precision == "float" else np.float64
@@ -51,17 +43,9 @@ def test_contiguous(Solver, periodicity):
     ],
 )
 def test_returns_mf(Solver, periodicity):
-    hydrodynamicRadius = 1.0
-    solver = Solver(*periodicity)
-    parameters = sane_parameters[Solver.__name__]
-    solver.setParameters(**parameters)
+
     numberParticles = 1
-    solver.initialize(
-        temperature=1.0,
-        viscosity=1.0,
-        hydrodynamicRadius=hydrodynamicRadius,
-        numberParticles=numberParticles,
-    )
+    solver = initialize_solver(Solver, periodicity, numberParticles)
 
     # Set precision to be the same as compiled precision
     precision = np.float32 if Solver.precision == "float" else np.float64
@@ -85,18 +69,9 @@ def test_returns_mf(Solver, periodicity):
     ],
 )
 def test_returns_mf_mt(Solver, periodicity):
-    hydrodynamicRadius = 1.0
-    solver = Solver(*periodicity)
-    parameters = sane_parameters[Solver.__name__]
-    solver.setParameters(**parameters)
+
     numberParticles = 1
-    solver.initialize(
-        temperature=1.0,
-        viscosity=1.0,
-        hydrodynamicRadius=hydrodynamicRadius,
-        numberParticles=numberParticles,
-        needsTorque=True
-    )
+    solver = initialize_solver(Solver, periodicity, numberParticles, needsTorque=True)
 
     # Set precision to be the same as compiled precision
     precision = np.float32 if Solver.precision == "float" else np.float64
@@ -126,17 +101,9 @@ def test_returns_mf_mt(Solver, periodicity):
     ],
 )
 def test_returns_sqrtM(Solver, periodicity):
-    hydrodynamicRadius = 1.0
-    solver = Solver(*periodicity)
-    parameters = sane_parameters[Solver.__name__]
-    solver.setParameters(**parameters)
+
     numberParticles = 1
-    solver.initialize(
-        temperature=1.0,
-        viscosity=1.0,
-        hydrodynamicRadius=hydrodynamicRadius,
-        numberParticles=numberParticles,
-    )
+    solver = initialize_solver(Solver, periodicity, numberParticles)
 
     # Set precision to be the same as compiled precision
     precision = np.float32 if Solver.precision == "float" else np.float64
@@ -179,3 +146,62 @@ def test_returns_hydrodisp(Solver, periodicity):
     assert sqrtmw.shape == (numberParticles, 3)
     sqrtmw, _ = solver.hydrodynamicVelocities(forces)
     assert sqrtmw.shape == (numberParticles, 3)
+
+@pytest.mark.parametrize(
+    ("Solver", "periodicity"),
+    [
+        (SelfMobility, ("open", "open", "open")),
+        (NBody, ("open", "open", "open")),
+        (NBody, ("open", "open", "single_wall")),
+        (DPStokes, ("periodic", "periodic", "open")),
+        (DPStokes, ("periodic", "periodic", "single_wall")),
+        (DPStokes, ("periodic", "periodic", "two_walls")),
+    ],
+)
+def test_no_torques_error(Solver, periodicity):
+    # Test that the solver raises an error if torques are provided but solver was not initialized with needsTorque=True
+    numberParticles = 1
+    solver = initialize_solver(Solver, periodicity, numberParticles, needsTorque=False)
+
+    # Set precision to be the same as compiled precision
+    precision = np.float32 if Solver.precision == "float" else np.float64
+
+    forces = np.random.rand(numberParticles, 3).astype(precision)
+    torques = np.random.rand(numberParticles, 3).astype(precision)
+    posistions = np.random.rand(numberParticles, 3).astype(precision)
+
+    solver.setPositions(posistions)
+
+    with pytest.raises(RuntimeError):
+        u, w = solver.Mdot(forces, torques)
+
+@pytest.mark.parametrize(
+    ("Solver", "periodicity"),
+    [
+        (SelfMobility, ("open", "open", "open")),
+        (NBody, ("open", "open", "open")),
+        (NBody, ("open", "open", "single_wall")),
+        (PSE, ("periodic", "periodic", "periodic")),
+        (DPStokes, ("periodic", "periodic", "open")),
+        (DPStokes, ("periodic", "periodic", "single_wall")),
+        (DPStokes, ("periodic", "periodic", "two_walls")),
+    ],
+)
+def test_no_positions_error(Solver, periodicity):
+    # Test that the solver raises an error if Mdot, sqrtMdot, and hydroDisp are called before setting positions
+    numberParticles = 1
+    solver = initialize_solver(Solver, periodicity, numberParticles)
+
+    # Set precision to be the same as compiled precision
+    precision = np.float32 if Solver.precision == "float" else np.float64
+
+    forces = np.random.rand(numberParticles, 3).astype(precision)
+
+    with pytest.raises(RuntimeError):
+        u, _ = solver.Mdot(forces)
+
+    with pytest.raises(RuntimeError):
+        sqrtmw, _ = solver.sqrtMdotW()
+
+    with pytest.raises(RuntimeError):
+        u, _ = solver.hydrodynamicVelocities()
