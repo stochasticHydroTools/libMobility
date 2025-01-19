1e664524e7f19157a9b7c7c9c3c33e86f13b87cb
Raul P. Pelaez <raulppelaez@gmail.com>
2025-01-19 09:54:49 +0100

    Fix some tests. Make sure NBody wall tests use particles far away from the wall

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -6,6 +6,7 @@ from utils import (
     initialize_solver,
     solver_configs_all,
     solver_configs_torques,
+    generate_positions_in_box,
 )
 
 
@@ -47,11 +48,14 @@ def test_returns_mf(Solver, periodicity):
 @pytest.mark.parametrize(("Solver", "periodicity"), solver_configs_torques)
 def test_returns_mf_mt(Solver, periodicity):
     numberParticles = 1
-    solver = initialize_solver(Solver, periodicity, numberParticles, needsTorque=True)
+    parameters = sane_parameters[Solver.__name__]
+    solver = initialize_solver(
+        Solver, periodicity, numberParticles, needsTorque=True, parameters=parameters
+    )
 
     # Set precision to be the same as compiled precision
     precision = np.float32 if Solver.precision == "float" else np.float64
-    positions = np.random.rand(numberParticles, 3).astype(precision)
+    positions = generate_positions_in_box(parameters, numberParticles)
     forces = np.random.rand(numberParticles, 3).astype(precision)
     torques = np.random.rand(numberParticles, 3).astype(precision)
     solver.setPositions(positions)
@@ -69,11 +73,12 @@ def test_returns_mf_mt(Solver, periodicity):
 def test_returns_sqrtM(Solver, periodicity):
 
     numberParticles = 1
-    solver = initialize_solver(Solver, periodicity, numberParticles)
-
+    parameters = sane_parameters[Solver.__name__]
+    solver = initialize_solver(
+        Solver, periodicity, numberParticles, parameters=parameters, temperature=1.0
+    )
     # Set precision to be the same as compiled precision
-    precision = np.float32 if Solver.precision == "float" else np.float64
-    positions = np.random.rand(numberParticles, 3).astype(precision)
+    positions = generate_positions_in_box(parameters, numberParticles)
     solver.setPositions(positions)
     sqrtmw, _ = solver.sqrtMdotW()
     assert sqrtmw.shape == (numberParticles, 3)
@@ -81,21 +86,14 @@ def test_returns_sqrtM(Solver, periodicity):
 
 @pytest.mark.parametrize(("Solver", "periodicity"), solver_configs_all)
 def test_returns_hydrodisp(Solver, periodicity):
-    hydrodynamicRadius = 1.0
-    solver = Solver(*periodicity)
     parameters = sane_parameters[Solver.__name__]
-    solver.setParameters(**parameters)
     numberParticles = 1
-    solver.initialize(
-        temperature=1.0,
-        viscosity=1.0,
-        hydrodynamicRadius=hydrodynamicRadius,
-        numberParticles=numberParticles,
+    solver = initialize_solver(
+        Solver, periodicity, numberParticles, parameters=parameters, temperature=1.0
     )
-
     # Set precision to be the same as compiled precision
     precision = np.float32 if Solver.precision == "float" else np.float64
-    positions = np.random.rand(numberParticles, 3).astype(precision)
+    positions = generate_positions_in_box(parameters, numberParticles)
     forces = np.random.rand(numberParticles, 3).astype(precision)
     solver.setPositions(positions)
     sqrtmw, _ = solver.hydrodynamicVelocities()
@@ -123,19 +121,11 @@ def test_no_torques_error(Solver, periodicity):
         u, w = solver.Mdot(forces, torques)
 
 
-@pytest.mark.parametrize(
-    ("Solver", "periodicity"),
-    [
-        (NBody, ("open", "open", "open")),
-        (NBody, ("open", "open", "single_wall")),
-        (PSE, ("periodic", "periodic", "periodic")),
-        (DPStokes, ("periodic", "periodic", "open")),
-        (DPStokes, ("periodic", "periodic", "single_wall")),
-        (DPStokes, ("periodic", "periodic", "two_walls")),
-    ],
-)
+@pytest.mark.parametrize(("Solver", "periodicity"), solver_configs_all)
 def test_no_positions_error(Solver, periodicity):
     # Test that the solver raises an error if Mdot, sqrtMdot, and hydroDisp are called before setting positions
+    if Solver.__name__ == "SelfMobility":
+        pytest.skip("SelfMobility does not require positions")
     numberParticles = 1
     solver = initialize_solver(Solver, periodicity, numberParticles)
 
@@ -144,13 +134,13 @@ def test_no_positions_error(Solver, periodicity):
 
     forces = np.random.rand(numberParticles, 3).astype(precision)
 
-    with pytest.raises(RuntimeError, match=r"\[libMobility\]*"):
+    with pytest.raises(RuntimeError):
         u, _ = solver.Mdot(forces)
 
-    with pytest.raises(RuntimeError, match=r"\[libMobility\]*"):
+    with pytest.raises(RuntimeError):
         sqrtmw, _ = solver.sqrtMdotW()
 
-    with pytest.raises(RuntimeError, match=r"\[libMobility\]*"):
+    with pytest.raises(RuntimeError):
         u, _ = solver.hydrodynamicVelocities()
 
 
