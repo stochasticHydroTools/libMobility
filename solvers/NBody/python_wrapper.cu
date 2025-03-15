54517bd7255f99b377460306945a7e0324b8c466
Ryker Fish <rfish@mines.edu>
2025-03-15 14:58:36 -0600

    docstring for nbody with new param

--- solvers/NBody/python_wrapper.cu
+++ solvers/NBody/python_wrapper.cu
@@ -3,6 +3,21 @@
 #include "mobility.h"
 #include <MobilityInterface/pythonify.h>
 
+static const char *docstringSetParameters = R"pbdoc(
+        Set the parameters for the NBody solver.
+
+        Parameters
+        ----------
+        algorithm : str
+                The algorithm to use. Options are "naive", "fast", "block" and "advise". Default is "advise".
+        NBatch : int
+                The number of batches to use. If -1 (default), the number of batches is automatically determined.
+        NperBatch : int
+                The number of particles per batch. If -1 (default), the number of particles per batch is automatically determined.
+        wallHeight : float
+                The height of the wall. Only valid if periodicityZ is single_wall. Default is 0.0.
+        )pbdoc";
+
 namespace nbody_rpy {
 auto string2NBodyAlgorithm(std::string algo) {
   if (algo == "naive")
@@ -28,6 +43,7 @@ MOBILITY_PYTHONIFY_WITH_EXTRA_CODE(
             myself.setParametersNBody(
                 {nbody_rpy::string2NBodyAlgorithm(algo), NBatch, NperBatch, wallHeight});
         },
+        docstringSetParameters,
         "algorithm"_a = "advise", "Nbatch"_a = -1, "NperBatch"_a = -1, "wallHeight"_a = 0.0);
     , "This module computes the RPY mobility using an N^2 algorithm in the "
       "GPU. Different hydrodynamic kernels can be chosen.");
