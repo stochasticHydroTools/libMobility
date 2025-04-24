dc368d18c0af129ce8fb78f2355aa0b391455f9e
Raul P. Pelaez <raulppelaez@gmail.com>
2025-04-24 13:43:23 +0200

    Update tests

--- tests/test_thermal_drift.py
+++ tests/test_thermal_drift.py
@@ -9,30 +9,65 @@ from utils import (
 )
 
 
-def accurate_average(current_mean, current_count, new_value):
+def online_average(current_mean, current_count, new_value):
     # Update the mean and count
     new_mean = current_mean + (new_value - current_mean) / (current_count + 1)
     return new_mean
 
 
+def average(function, num_averages):
+    result = function()
+    for i in range(num_averages - 1):
+        result += function()
+    return result / num_averages
+
+
 def thermal_drift_rfd(solver, positions):
     # RFD works by approxmating kT\partial_q \dot M = 1/\delta \langle M(q+\delta/2 W)W - M(q-\delta/2 W)W \rangle
     # Where delta is a small number, and W is a normal random vector of unit length
-    tdrift = np.zeros_like(positions)
     delta = 1e-4
-    for i in range(30000):
+
+    def thermal_drift_func():
         W = np.random.normal(size=positions.shape)
-        qpd = positions + delta / 2 * W
-        solver.setPositions(qpd)
-        dMf, _ = solver.Mdot(W)
-        _tdrift = dMf
-        qmd = positions - delta / 2 * W
-        solver.setPositions(qmd)
-        dMf, _ = solver.Mdot(W)
-        _tdrift -= dMf
+        solver.setPositions(positions + delta / 2 * W)
+        _tdrift = solver.Mdot(W)[0]
+        solver.setPositions(positions - delta / 2 * W)
+        _tdrift -= solver.Mdot(W)[0]
         _tdrift /= delta
-        tdrift = accurate_average(tdrift, i, _tdrift)
+        return _tdrift
+
+    tdrift = average(lambda: average(thermal_drift_func, 100), 100)
+    solver.setPositions(positions)
     return tdrift
+
+
+@pytest.mark.parametrize(("Solver", "periodicity"), solver_configs_all)
+def test_thermal_drift_does_not_change_positions(Solver, periodicity):
+    # Compute the Mdot with some random forces, then compute the thermal drift, then compute Mdot again
+    # Check that Mdot has not changed
+    needsTorque = False
+    precision = np.float32 if Solver.precision == "float" else np.float64
+    solver = Solver(*periodicity)
+    parameters = get_sane_params(Solver.__name__, periodicity[2])
+    solver.setParameters(**parameters)
+    solver.initialize(
+        temperature=1.0,
+        viscosity=1.0,
+        hydrodynamicRadius=1.0,
+        needsTorque=needsTorque,
+    )
+    positions = generate_positions_in_box(parameters, 10).astype(precision)
+    solver.setPositions(positions)
+    forces = np.random.normal(size=positions.shape).astype(precision)
+    mf, _ = solver.Mdot(forces)
+    solver.thermalDrift()
+    mf2, _ = solver.Mdot(forces)
+    assert np.allclose(
+        mf,
+        mf2,
+        atol=1e-7,
+        rtol=1e-7,
+    ), f"Mdot has changed: {np.max(np.abs(mf - mf2))}"
 
 
 @pytest.mark.parametrize(("Solver", "periodicity"), solver_configs_all)
@@ -54,20 +89,20 @@ def test_thermal_drift_is_zero(
     parameters = get_sane_params(Solver.__name__, periodicity[2])
     solver.setParameters(**parameters)
     solver.initialize(
-        temperature=0.0,
+        temperature=1.0,
         viscosity=1.0,
         hydrodynamicRadius=hydrodynamicRadius,
         needsTorque=needsTorque,
     )
     positions = generate_positions_in_box(parameters, numberParticles).astype(precision)
     solver.setPositions(positions)
-    reference = thermal_drift_rfd(solver, positions)
+    thermal_drift = average(solver.thermalDrift, 3000)
     assert np.allclose(
-        np.abs(reference),
+        np.abs(thermal_drift),
         0.0,
         atol=1e-5,
         rtol=0,
-    ), f"RFD drift is not zero: {np.max(np.abs(reference))}"
+    ), f"RFD drift is not zero: {np.max(np.abs(thermal_drift))}"
 
 
 @pytest.mark.parametrize(("Solver", "periodicity"), solver_configs_all)
@@ -76,21 +111,24 @@ def test_thermal_drift_is_zero(
 def test_thermal_drift_matches_rfd(
     Solver, periodicity, hydrodynamicRadius, numberParticles
 ):
+    temperature = 1.2
     needsTorque = False
     precision = np.float32 if Solver.precision == "float" else np.float64
     solver = Solver(*periodicity)
     parameters = get_sane_params(Solver.__name__, periodicity[2])
     solver.setParameters(**parameters)
     solver.initialize(
-        temperature=0.0,
+        temperature=temperature,
         viscosity=1.0,
         hydrodynamicRadius=hydrodynamicRadius,
         needsTorque=needsTorque,
     )
-    positions = generate_positions_in_box(parameters, numberParticles).astype(precision)
-    reference = thermal_drift_rfd(solver, positions)
+    positions = np.asarray(
+        generate_positions_in_box(parameters, numberParticles).astype(precision) * 0.8
+    )
+    reference = temperature * thermal_drift_rfd(solver, positions)
     solver.setPositions(positions)
-    rfd = solver.thermalDrift()
+    rfd = average(lambda: average(solver.thermalDrift, 100), 1000)
     assert np.allclose(
         reference,
         rfd,
