f925731c9074718fa9054da2905bd582d8d3f88d
Ryker Fish <rfish@mines.edu>
2025-08-15 12:57:50 -0600

    add simple test for isotropy in dpstokes, it's failing

--- tests/test_dpstokes.py
+++ tests/test_dpstokes.py
@@ -37,3 +37,22 @@ def test_non_square_box():
     assert np.allclose(
         mf_dp_ly, mf_dp_cube, rtol=1e-3, atol=1e-2
     ), "DPStokes ly=32.0 does not match cube"
+
+
+def test_isotropy():
+    L0 = 16.0
+    L_fact = 2.0
+
+    f_x = np.array([1.0, 0.0, 0.0])
+    f_y = np.array([0.0, 1.0, 0.0])
+
+    n_runs = 5
+    for _ in range(n_runs):
+        pos = np.random.uniform(-8, 8, (3)).astype(np.float32)
+
+        mf_x = compute_with_dpstokes(pos, f_x, lx=L0 * L_fact, ly=L0)
+        mf_y = compute_with_dpstokes(pos, f_y, lx=L0, ly=L0 * L_fact)
+
+        assert np.allclose(mf_x[0], mf_y[1], rtol=1e-3, atol=1e-2)
+        assert np.allclose(mf_x[1], mf_y[0], rtol=1e-3, atol=1e-2)
+        assert np.allclose(mf_x[2], mf_y[2], rtol=1e-3, atol=1e-2)
