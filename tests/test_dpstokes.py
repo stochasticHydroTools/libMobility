1715f6cd16e23116f2cc5e43c30c59eaf9ba64a0
Ryker Fish <rfish@mines.edu>
2025-08-15 20:00:57 -0600

    made box test more flexible

--- tests/test_dpstokes.py
+++ tests/test_dpstokes.py
@@ -20,23 +20,35 @@ def test_non_square_box():
     forces_or = np.random.uniform(-1, 1, (nP, 3)).astype(np.float32)
     forces_or -= np.mean(forces_or, axis=0)  # Center the forces around zero
 
-    mf_dp_cube = compute_with_dpstokes(pos_or, forces_or, lx=16.0, ly=16.0)
+    L_mult = 1.0
+    L_short = 16.0 * L_mult
+    L_fact = 2.0 * L_mult
+    L_long = L_short * L_fact
+
+    mf_dp_cube = compute_with_dpstokes(pos_or, forces_or, lx=L_short, ly=L_short)
 
     # This must be identical than doubling the box size in x and repeating the particles
-    pos = np.vstack((pos_or, pos_or + np.array([16.0, 0.0, 0.0])))
+    pos = np.vstack((pos_or, pos_or + np.array([L_short, 0.0, 0.0])))
     forces = np.vstack((forces_or, forces_or))
-    mf_dp_lx = compute_with_dpstokes(pos, forces, lx=32.0, ly=16.0)[: len(pos_or), :]
+    mf_dp_lx = compute_with_dpstokes(pos, forces, lx=L_long, ly=L_short)[
+        : len(pos_or), :
+    ]
 
     # And the same for doubling the box size in y
-    pos = np.vstack((pos_or, pos_or + np.array([0.0, 16.0, 0.0])))
+    pos = np.vstack((pos_or, pos_or + np.array([0.0, L_short, 0.0])))
     forces = np.vstack((forces_or, forces_or))
-    mf_dp_ly = compute_with_dpstokes(pos, forces, lx=16.0, ly=32.0)[: len(pos_or), :]
-    assert np.allclose(
-        mf_dp_lx, mf_dp_cube, rtol=1e-3, atol=1e-2
-    ), "DPStokes lx=32.0 does not match cube"
-    assert np.allclose(
-        mf_dp_ly, mf_dp_cube, rtol=1e-3, atol=1e-2
-    ), "DPStokes ly=32.0 does not match cube"
+    mf_dp_ly = compute_with_dpstokes(pos, forces, lx=L_short, ly=L_long)[
+        : len(pos_or), :
+    ]
+    lx_match = np.allclose(mf_dp_lx, mf_dp_cube, rtol=1e-3, atol=1e-2)
+    ly_match = np.allclose(mf_dp_ly, mf_dp_cube, rtol=1e-3, atol=1e-2)
+
+    if not lx_match:
+        print("DPStokes lx=32.0 does not match cube")
+    if not ly_match:
+        print("DPStokes ly=32.0 does not match cube")
+
+    assert lx_match and ly_match, "DPStokes non-square box test failed"
 
 
 def test_isotropy():
