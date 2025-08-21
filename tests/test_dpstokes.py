c5777893b57074251e88a00878d2700d04b259bb
Ryker Fish <rfish@mines.edu>
2025-08-20 21:51:46 -0600

    modified tests for torques

--- tests/test_dpstokes.py
+++ tests/test_dpstokes.py
@@ -2,16 +2,18 @@ import numpy as np
 import libMobility as lm
 
 
-def compute_with_dpstokes(pos, forces, lx, ly):
+def compute_with_dpstokes(pos, lx, ly, forces=None, torques=None):
     params = {"viscosity": 1 / (6 * np.pi), "hydrodynamicRadius": 1.73}
-    dpstokes = lm.DPStokes(
-        periodicityX="periodic", periodicityY="periodic", periodicityZ="single_wall"
-    )
+    dpstokes = lm.DPStokes("periodic", "periodic", "single_wall")
+    torques_on = True if torques is not None else False
     dpstokes.setParameters(Lx=lx, Ly=ly, zmin=-8.0, zmax=8.0)
-    dpstokes.initialize(**params)
+    dpstokes.initialize(**params, includeAngular=torques_on)
     dpstokes.setPositions(pos)
-    mf_dp, _ = dpstokes.Mdot(forces=forces)
-    return mf_dp
+    mf_dp, mt_dp = dpstokes.Mdot(forces=forces, torques=torques)
+    if torques_on:
+        return mt_dp
+    else:
+        return mf_dp
 
 
 def test_non_square_box():
@@ -24,30 +26,36 @@ def test_non_square_box():
     L_short = 16.0
     L_long = L_short * L_fact
 
-    mf_dp_cube = compute_with_dpstokes(pos_or, forces_or, lx=L_short, ly=L_short)
+    mf_dp_cube = compute_with_dpstokes(pos_or, forces=forces_or, lx=L_short, ly=L_short)
+    mt_dp_cube = compute_with_dpstokes(
+        pos_or, torques=forces_or, lx=L_short, ly=L_short
+    )
 
     # This must be identical than doubling the box size in x and repeating the particles
     pos = np.vstack((pos_or, pos_or + np.array([0.5 * L_long, 0.0, 0.0])))
     forces = np.vstack((forces_or, forces_or))
-    mf_dp_lx = compute_with_dpstokes(pos, forces, lx=L_long, ly=L_short)[
+    mf_dp_lx = compute_with_dpstokes(pos, forces=forces, lx=L_long, ly=L_short)[
+        : len(pos_or), :
+    ]
+    mt_dp_lx = compute_with_dpstokes(pos, torques=forces, lx=L_long, ly=L_short)[
         : len(pos_or), :
     ]
 
     # And the same for doubling the box size in y
     pos = np.vstack((pos_or, pos_or + np.array([0.0, 0.5 * L_long, 0.0])))
     forces = np.vstack((forces_or, forces_or))
-    mf_dp_ly = compute_with_dpstokes(pos, forces, lx=L_short, ly=L_long)[
+    mf_dp_ly = compute_with_dpstokes(pos, forces=forces, lx=L_short, ly=L_long)[
         : len(pos_or), :
     ]
-    lx_match = np.allclose(mf_dp_lx, mf_dp_cube, rtol=1e-3, atol=1e-2)
-    ly_match = np.allclose(mf_dp_ly, mf_dp_cube, rtol=1e-3, atol=1e-2)
+    mt_dp_ly = compute_with_dpstokes(pos, torques=forces, lx=L_short, ly=L_long)[
+        : len(pos_or), :
+    ]
 
-    if not lx_match:
-        print("DPStokes lx=32.0 does not match cube")
-    if not ly_match:
-        print("DPStokes ly=32.0 does not match cube")
+    assert np.allclose(mf_dp_lx, mf_dp_cube, rtol=1e-3, atol=1e-2)
+    assert np.allclose(mf_dp_ly, mf_dp_cube, rtol=1e-3, atol=1e-2)
 
-    assert lx_match and ly_match, "DPStokes non-square box test failed"
+    assert np.allclose(mt_dp_lx, mt_dp_cube, rtol=1e-3, atol=1e-2)
+    assert np.allclose(mt_dp_ly, mt_dp_cube, rtol=1e-3, atol=1e-2)
 
 
 def test_isotropy():
@@ -61,12 +69,19 @@ def test_isotropy():
     for _ in range(n_runs):
         pos = np.random.uniform(-8, 8, (3)).astype(np.float32)
 
-        mf_x = compute_with_dpstokes(pos, f_x, lx=L0 * L_fact, ly=L0)
-        mf_y = compute_with_dpstokes(pos, f_y, lx=L0, ly=L0 * L_fact)
+        mf_x = compute_with_dpstokes(pos, forces=f_x, lx=L0 * L_fact, ly=L0)
+        mf_y = compute_with_dpstokes(pos, forces=f_y, lx=L0, ly=L0 * L_fact)
+
+        mt_x = compute_with_dpstokes(pos, torques=f_x, lx=L0 * L_fact, ly=L0)
+        mt_y = compute_with_dpstokes(pos, torques=f_y, lx=L0, ly=L0 * L_fact)
 
         assert np.allclose(mf_x[0], mf_y[1], rtol=1e-3, atol=1e-2)
         assert np.allclose(mf_x[1], mf_y[0], rtol=1e-3, atol=1e-2)
         assert np.allclose(mf_x[2], mf_y[2], rtol=1e-3, atol=1e-2)
+
+        assert np.allclose(mt_x[0], mt_y[1], rtol=1e-3, atol=1e-2)
+        assert np.allclose(mt_x[1], mt_y[0], rtol=1e-3, atol=1e-2)
+        assert np.allclose(mt_x[2], mt_y[2], rtol=1e-3, atol=1e-2)
 
 
 def test_non_square_matching_rpy():
@@ -74,39 +89,46 @@ def test_non_square_matching_rpy():
     a = np.random.uniform(0.5, 2.0)
     eta = 1 / (6 * np.pi * a)
     L_min = 100 * a  # need a fairly large domain to neglect periodic effects
-    Lx = np.random.uniform(L_min, 4.0 * L_min)
-    Ly = np.random.uniform(L_min, 4.0 * L_min)
+    Lx = np.random.uniform(L_min, 3.0 * L_min)
+    Ly = np.random.uniform(L_min, 3.0 * L_min)
     max_height = 10.0 * a
     min_height = 1.5 * a  # need a buffer for regularized kernels to match
 
     solver = lm.DPStokes("periodic", "periodic", "single_wall")
     solver.setParameters(Lx=Lx, Ly=Ly, zmin=0.0, zmax=max_height)
-    solver.initialize(hydrodynamicRadius=a, viscosity=eta)
+    solver.initialize(hydrodynamicRadius=a, viscosity=eta, includeAngular=True)
 
     rpy = lm.NBody("open", "open", "single_wall")
     rpy.setParameters(wallHeight=0.0)
-    rpy.initialize(hydrodynamicRadius=a, viscosity=eta)
+    rpy.initialize(hydrodynamicRadius=a, viscosity=eta, includeAngular=True)
 
-    F = np.eye(3)
+    F = np.eye(6)
 
     heights = np.linspace(min_height, max_height, 20)
 
-    results_dpstokes = np.zeros((len(heights), 3))
-    results_rpy = np.zeros((len(heights), 3))
+    results_dpstokes = np.zeros((2*len(heights), 3))
+    results_rpy = np.zeros((2*len(heights), 3))
+
+    breakpoint()
 
     for i, h in enumerate(heights):
         pos = np.array([0.0, 0.0, h])
 
         rpy.setPositions(pos)
         solver.setPositions(pos)
-        for j in range(3):
-            f_j = F[:, j].copy()
-            mf_j = solver.Mdot(forces=f_j)[0]
+        for j in range(6):
+            f_j = F[0:3, j].copy()
+            t_j = F[3:6, j].copy()
+            mf_j, mt_j = solver.Mdot(forces=f_j, torques=t_j)
             results_dpstokes[i] += mf_j
+            results_dpstokes[i+3] += mt_j
 
-            mf_j = rpy.Mdot(forces=f_j)[0]
+            mf_j, mt_j = rpy.Mdot(forces=f_j, torques=t_j)
             results_rpy[i] += mf_j
+            results_rpy[i+3] += mt_j
 
-    assert np.allclose(results_dpstokes[:, 0], results_rpy[:, 0], rtol=1e-3, atol=1e-2)
-    assert np.allclose(results_dpstokes[:, 1], results_rpy[:, 1], rtol=1e-3, atol=1e-2)
-    assert np.allclose(results_dpstokes[:, 2], results_rpy[:, 2], rtol=1e-3, atol=1e-2)
+    assert np.allclose(results_dpstokes, results_rpy, rtol=1e-3, atol=1e-2)
+
+    # assert np.allclose(results_dpstokes[:, 0], results_rpy[:, 0], rtol=1e-3, atol=1e-2)
+    # assert np.allclose(results_dpstokes[:, 1], results_rpy[:, 1], rtol=1e-3, atol=1e-2)
+    # assert np.allclose(results_dpstokes[:, 2], results_rpy[:, 2], rtol=1e-3, atol=1e-2)
